cf2fcba1c583b85eb95a3bcb4d0c631eef4724c3
FABIAN GOMEZ <fabian.gomez.2310@gmail.com>
2025-04-19 15:23:11 -0500

    Se crea el archivo resta y se agrega al main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const sumar = require('./suma');
 
+const restar = require('./resta');
+
 console.log("Calculadora iniciada");
 
 console.log("Resultado de la suma: "+sumar(5,3));
+console.log("Resultado de la resta: " +restar(5,3));
