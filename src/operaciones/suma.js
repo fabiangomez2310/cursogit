ec1412c5938e01b703e6e38960717daa723777f7
FABIAN GOMEZ <fabian.gomez.2310@gmail.com>
2025-04-21 14:33:48 -0500

    Actualización en operaciones de suma y resta, y ajustes en principal.js

--- src/operaciones/suma.js
+++ src/operaciones/suma.js
@@ -1,5 +1,9 @@
 function sumar (a, b) {
-    return a+b;
+    if(typeof a !== "number" || typeof b!== "number"){
+        return "Error: ambos argumentos deben ser numeros."
+    }else{
+        return a+b;
+    }
 }
 
 export default sumar;
--- src/principal.js
+++ src/principal.js
@@ -1,15 +1,16 @@
-import sumar from './operaciones/suma';
+import sumar from "./operaciones/suma";
 
-import restar from './operaciones/resta';
+import restar from "./operaciones/resta";
 
-import multiplicar from './operaciones/multiplicacion';
+import multiplicar from "./operaciones/multiplicacion";
+import dividir from "./operaciones/division";
 
 console.log("Calculadora iniciada");
 
-let num1= 5;
-let num2= 3;
+let num1 = 5;
+let num2 = 3;
 
-console.log("Resultado de la suma: "+sumar(num1,num2));
-console.log("Resultado de la resta: " +restar(num1,num2));
-console.log("Resultado de la multiplicacion: " +multiplicar(num1, num2));
-
+console.log("Resultado de la suma: " + sumar(num1, num2));
+console.log("Resultado de la resta: " + restar(num1, num2));
+console.log("Resultado de la multiplicacion: " + multiplicar(num1, num2));
+console.log("Resultado de la division: "+dividir(num1,num2));
