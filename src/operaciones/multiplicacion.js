415923f83eb5e5802f1ddd8dd91b0b6e243b420f
FABIAN GOMEZ <fabian.gomez.2310@gmail.com>
2025-04-21 14:15:18 -0500

    Se agrega validacion a multiplicacion

--- src/operaciones/multiplicacion.js
+++ src/operaciones/multiplicacion.js
@@ -1,5 +1,10 @@
 function multiplicar(a,b){
-    return a*b;
+    if(typeof a !== "number" || typeof b!== "number"){
+        return "Error: ambos argumentos deben ser numeros."
+    }else{
+        return a*b;
+    }
+    
 }
 
 export default multiplicar;
